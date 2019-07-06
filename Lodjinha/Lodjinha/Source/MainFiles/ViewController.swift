a8dbc2849005ab7fe1327ab453a548be6d07a206
Roger Sanoli <rogersanoli.app@gmail.com>
2019-07-06 15:02:25 -0300

    Initial Commit: Adding frameworks, XCAssets and GitIgnore

---
+++ Lodjinha/Lodjinha/Source/MainFiles/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Lodjinha
+//
+//  Created by Roger Sanoli on 05/07/19.
+//  Copyright © 2019 RogerSanoli. All rights reserved.
+//
+
+import UIKit
+import PaladinKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
